e545a75e0fcf78279c7cbf336df8e20b9f25bcfd
restbackDev <jason.rbdev@gmail.com>
2025-03-17 12:17:42 -0700

    wip:edited gtiignore,passuser,mounted signIn/passUser

--- server.js
+++ server.js
@@ -7,16 +7,18 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+//middleware
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
@@ -28,15 +30,18 @@ app.use(
   })
 );
 
+//MUST BE BELOW app.use session
+app.use(passUserToView); //passUserView comes after session middleware but before homepage
+//MUST BE ABOVE app.get "/"
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
   });
 });
 
-
-
 app.use('/auth', authController);
+app.use(isSignedIn);//MUST BE BELOW app.use "/auth"
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
